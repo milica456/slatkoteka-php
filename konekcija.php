aeb6a4a60411406450a806fccc383a9581f0e50d
Milica Jaksic <mj20190209@student.fon.bg.ac.rs>
2023-03-18 21:10:30 +0100

    Napravljena konekcija sa bazom

---
+++ konekcija.php
@@ -0,0 +1,9 @@
+<?php
+  error_reporting(E_ALL | E_STRICT);
+  ini_set("display_errors", true);
+  ini_set("log_errors", true);
+
+
+  $conn = new MySqli('localhost', 'root','','slatkoteka');
+  $conn->set_charset("utf8");
+ ?>
